d81bfc2f0d55c0a7c4abdbec418c340d6b05067a
Ryland Mathews <Rmathews@cwp.local>
2020-02-24 14:30:54 -0500

    frontend test

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,95 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 import Navbar from "./components/navbar.component"
 import ChartComponent from "./components/reacths.component"
 
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
 
-function App() {
- return (
-  <Router>
-    <div> 
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-          <div className="navbar-brand">Machine Learning Models</div>
-          <div className="collpase navbar-collapse">
-          <ul className="navbar-nav mr-auto"></ul>
-          </div>
-        </nav>
-    <Switch>
-        <Route path='/spyml_chart'>
-            <div className="container">
-              <br/>
-              <ChartComponent />
-            </div>
-        </Route>
-    </Switch>
-    </div>
-  </Router>
- );
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
- 
-export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
+
+
