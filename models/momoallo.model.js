efc9c6d9a21395ed1e1ffdcab7cbb8e97b913c13
Ryland Mathews <Rmathews@cwp.local>
2020-03-24 14:07:03 -0400

    allocation edits

--- models/momoallo.model.js
+++ models/momoallo.model.js
@@ -5,6 +5,11 @@ const momoalloSchema = new Schema({
     index: { type: String, required: true },
     value: { type: Number, required: true },
     pnl: { type: Number, required: true },
+    cost: { type: Number, required: true },
+    shares: { type: Number, required: true },
+    pct_allo: { type: Number, required: true },
+    pct_pnl: { type: Number, required: true },
+    portfolio_pct_pnl: { type: Number, required: true },
 }, {
   timestamps: true,
   collection: 'momoallo'
