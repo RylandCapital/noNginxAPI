c597e77d8f9a7bd4fbd172926351a1df582166d3
Ryland Mathews <Rmathews@cwp.local>
2020-04-01 14:21:44 -0400

    backend updates

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")))
+
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true}
@@ -81,7 +81,7 @@ const mscoreRouter = require('./routes/mscore');
 const mscorefiveRouter = require('./routes/mscorefive');
 const mscoretenRouter = require('./routes/mscoreten');
 
-/*app.use('/', spxRouter);*/
+/*app.use('/',express.static(path.join(__dirname, "client/build")))*/
 app.use('/spx', spxRouter);
 app.use('/vix', vixRouter);
 app.use('/tlt', tltRouter);
