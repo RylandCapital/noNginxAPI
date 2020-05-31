a35c246a24779126f197ed6012e3b2ade9920cd8
Ryland Mathews <Rmathews@cwp.local>
2020-05-31 17:54:44 -0400

    pevents

--- models/pevents.model.js
+++ models/pevents.model.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const peventsSchema = new Schema({'ticker': { type: String, required: true },
-'signal': { type: String, required: true },
-'start_date': { type: Date, required: true },
-'end_date': { type: Date, required: true },
-'tf_of_signal': { type: Number, required: true },
-'cost_basis': { type: Number, required: true },
-'last': { type: Number, required: true },
-'PNL': { type: Number, required: true }
+const peventsSchema = new Schema({
 }, {
   timestamps: true,
   collection: 'pevents'
