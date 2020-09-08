52bb756b5993adb1b64ef025c14e6dfb6399b7f1
Ryland Mathews <Rmathews@cwp.local>
2020-09-08 12:04:34 -0400

    speed2

--- routes/agggroup.js
+++ routes/agggroup.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 let agggroup = require('../models/agggroup.model');
 
 router.route('/').get((req, res) => {
-    agggroup.find().lean()
+    agggroup.find()
         .then(agggroup => res.json(agggroup))
         .catch(err => res.status(400).json('Error: ' + err));
 });
