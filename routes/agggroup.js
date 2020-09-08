ee3aa5e9c9b9af66f359797479e713013abbaa15
Ryland Mathews <Rmathews@cwp.local>
2020-09-08 11:40:39 -0400

    speed improvements

--- routes/agggroup.js
+++ routes/agggroup.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 let agggroup = require('../models/agggroup.model');
 
 router.route('/').get((req, res) => {
-    agggroup.find()
+    agggroup.find().lean()
         .then(agggroup => res.json(agggroup))
         .catch(err => res.status(400).json('Error: ' + err));
 });
