9484db8966483b782994389feeb81bfa625b796c
Ryland <rylandmathews1@gmail.com>
2021-03-01 09:40:25 -0500

    ratio split

--- models/ratio2.model.js
+++ models/ratio2.model.js
@@ -1,22 +1,269 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { send } = require('process');
 
 
 
-const ratiofinancialsSchema = new Schema({
+//these endpoints are for tradingview ratio charts, sectorsd and subsectors
+
+//Sectors
+
+const materialsSchema = new Schema({
 }, {
   timestamps: true,
-  collection: 'ratio_finance'
+  collection: 'materials_ratio'
+});
+
+const consumer_staplesSchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'consumer_staples_ratio'
+});
+
+const consumer_discretionarySchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'consumer_discretionary_ratio'
+});
+
+const energySchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'energy_ratio'
+});
+
+const financialsSchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'financials_ratio'
+});
+
+const health_careSchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'health_care_ratio'
+});
+
+const industrialsSchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'industrials_ratio'
+});
+
+const real_estateSchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'real_estate_ratio'
+});
+
+const information_technologySchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'information_technology_ratio'
+});
+
+const communication_servicesSchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'communication_services_ratio'
+});
+
+const utilitiesSchema = new Schema({
+}, {
+  timestamps: true,
+  collection: 'utilities_ratio'
 });
 
 
+//GROUPS
+const interactivemediaentertainmentSchema = new Schema({}, {timestamps: true, collection:'interactivemediaentertainment_ratio'});
+const telecomSchema = new Schema({}, {timestamps: true, collection:'telecom_ratio'});
+const mediaentertainmentSchema = new Schema({}, {timestamps: true, collection:'mediaentertainment_ratio'});
+const apparelluxurygoodsSchema = new Schema({}, {timestamps: true, collection:'apparelluxurygoods_ratio'});
+const automobileSchema = new Schema({}, {timestamps: true, collection:'automobile_ratio'});
+const hotelrestaurantleisureSchema = new Schema({}, {timestamps: true, collection:'hotelrestaurantleisure_ratio'});
+const householddurablesSchema = new Schema({}, {timestamps: true, collection:'householddurables_ratio'});
+const internetdirectmarketingSchema = new Schema({}, {timestamps: true, collection:'internetdirectmarketing_ratio'});
+const leisuregoodsSchema = new Schema({}, {timestamps: true, collection:'leisuregoods_ratio'});
+const retailSchema = new Schema({}, {timestamps: true, collection:'retail'});
+const foodbeveragetobaccoSchema = new Schema({}, {timestamps: true, collection:'foodbeveragetobacco_ratio'});
+const foodstaplesretailingSchema = new Schema({}, {timestamps: true, collection:'foodstaplesretailing_ratio'});
+const householdpersonalproductsSchema = new Schema({}, {timestamps: true, collection:'householdpersonalproducts_ratio'});
+const oilgasconsumablefuelsSchema = new Schema({}, {timestamps: true, collection:'oilgasconsumablefuels_ratio'});
+const oilgasequipmentservicesSchema = new Schema({}, {timestamps: true, collection:'oilgasequipmentservices_ratio'});
+const oilgasexplorationproductionSchema = new Schema({}, {timestamps: true, collection:'oilgasexplorationproduction_ratio'});
+const oilgasstoragetransportationSchema = new Schema({}, {timestamps: true, collection:'oilgasstoragetransportation_ratio'});
+const banksSchema = new Schema({}, {timestamps: true, collection:'banks_ratio'});
+const brokeragecapitalmarketsSchema = new Schema({}, {timestamps: true, collection:'brokeragecapitalmarkets_ratio'});
+const diversifiedfinancialcreditservicesSchema = new Schema({}, {timestamps: true, collection:'diversifiedfinancialcreditservices_ratio'});
+const insuranceSchema = new Schema({}, {timestamps: true, collection:'insurance_ratio'});
+const regionalbanksSchema = new Schema({}, {timestamps: true, collection:'regionalbanks_ratio'});
+const thriftsmortgagefinanceSchema = new Schema({}, {timestamps: true, collection:'thriftsmortgagefinance_ratio'});
+const biotechnologySchema = new Schema({}, {timestamps: true, collection:'biotechnology_ratio'});
+const healthcareservicesSchema = new Schema({}, {timestamps: true, collection:'healthcareservices_ratio'});
+const medicalequipmentdevicessuppliesSchema = new Schema({}, {timestamps: true, collection:'medicalequipmentdevicessupplies_ratio'});
+const pharmaceuticalsSchema = new Schema({}, {timestamps: true, collection:'pharmaceuticals_ratio'});
+const aerospacedefenseSchema = new Schema({}, {timestamps: true, collection:'aerospacedefense_ratio'});
+const commercialprofessionalservicesSchema = new Schema({}, {timestamps: true, collection:'commercialprofessionalservices_ratio'});
+const constructionmaterialsSchema = new Schema({}, {timestamps: true, collection:'constructionmaterials_ratio'});
+const industrialgoodsSchema = new Schema({}, {timestamps: true, collection:'industrialgoods_ratio'});
+const machinerySchema = new Schema({}, {timestamps: true, collection:'machinery_ratio'});
+const transportationSchema = new Schema({}, {timestamps: true, collection:'transportation_ratio'});
+const hardwareequipmentSchema = new Schema({}, {timestamps: true, collection:'hardwareequipment_ratio'});
+const itservicesSchema = new Schema({}, {timestamps: true, collection:'itservices_ratio'});
+const semiconductorsSchema = new Schema({}, {timestamps: true, collection:'semiconductors_ratio'});
+const softwareSchema = new Schema({}, {timestamps: true, collection:'software_ratio'});
+const basicmaterialsSchema = new Schema({}, {timestamps: true, collection:'basicmaterials_ratio'});
+const industrialmetalsSchema = new Schema({}, {timestamps: true, collection:'industrialmetals_ratio'});
+const preciousmetalsSchema = new Schema({}, {timestamps: true, collection:'preciousmetals_ratio'});
+const commercialreitsSchema = new Schema({}, {timestamps: true, collection:'commercialreits_ratio'});
+const realestateservicesSchema = new Schema({}, {timestamps: true, collection:'realestateservices_ratio'});
+const residentialmortgagesSchema = new Schema({}, {timestamps: true, collection:'residentialmortgages_ratio'});
+const retailreitsSchema = new Schema({}, {timestamps: true, collection:'retailreits_ratio'});
+const electricSchema = new Schema({}, {timestamps: true, collection:'electric_ratio'});
+const gasSchema = new Schema({}, {timestamps: true, collection:'gas_ratio'});
+const independentpowerrenewablesSchema = new Schema({}, {timestamps: true, collection:'independentpowerrenewables_ratio'});
+const multilineutilitiesSchema = new Schema({}, {timestamps: true, collection:'multilineutilities_ratio'});
+const waterSchema = new Schema({}, {timestamps: true, collection:'water_ratio'});
+const chemicalsSchema = new Schema({}, {timestamps: true, collection:'chemicals_ratio'});
+const diversifiedconsumerservicesSchema = new Schema({}, {timestamps: true, collection:'diversifiedconsumerservices_ratio'});
 
-const ratiofinancials= mongoose.model('ratiofinancials', ratiofinancialsSchema);
 
+
+
+
+//Sectors
+const materials_ratio = mongoose.model('materials_ratio', materialsSchema);
+const consumer_staples_ratio  = mongoose.model('consumer_staples_ratio', consumer_staplesSchema);
+const consumer_discretionary_ratio  = mongoose.model('consumer_discretionary_ratio', consumer_discretionarySchema);
+const energy_ratio  = mongoose.model('energy_ratio', energySchema);
+const financials_ratio  = mongoose.model('financials_ratio', financialsSchema);
+const health_care_ratio  = mongoose.model('health_care_ratio', health_careSchema);
+const industrials_ratio  = mongoose.model('industrials_ratio', industrialsSchema);
+const real_estate_ratio  = mongoose.model('real_estate_ratio', real_estateSchema);
+const information_technology_ratio  = mongoose.model('information_technology_ratio', information_technologySchema);
+const communication_services_ratio  = mongoose.model('communication_services_ratio', communication_servicesSchema);
+const utilities_ratio  = mongoose.model('utilities_ratio', utilitiesSchema);
+
+
+
+//Groups
+const interactivemediaentertainment  = mongoose.model('interactivemediaentertainment_ratio', interactivemediaentertainmentSchema);
+const telecom  = mongoose.model('telecom_ratio', telecomSchema);
+const mediaentertainment  = mongoose.model('mediaentertainment_ratio', mediaentertainmentSchema);
+const apparelluxurygoods  = mongoose.model('apparelluxurygoods_ratio', apparelluxurygoodsSchema);
+const automobile  = mongoose.model('automobile_ratio', automobileSchema);
+const hotelrestaurantleisure  = mongoose.model('hotelrestaurantleisure_ratio', hotelrestaurantleisureSchema);
+const householddurables  = mongoose.model('householddurables_ratio', householddurablesSchema);
+const internetdirectmarketing  = mongoose.model('internetdirectmarketing_ratio', internetdirectmarketingSchema);
+const leisuregoods  = mongoose.model('leisuregoods_ratio', leisuregoodsSchema);
+const retail  = mongoose.model('retail_ratio', retailSchema);
+const foodbeveragetobacco  = mongoose.model('foodbeveragetobacco_ratio', foodbeveragetobaccoSchema);
+const foodstaplesretailing  = mongoose.model('foodstaplesretailing_ratio', foodstaplesretailingSchema);
+const householdpersonalproducts  = mongoose.model('householdpersonalproducts_ratio', householdpersonalproductsSchema);
+const oilgasconsumablefuels  = mongoose.model('oilgasconsumablefuels_ratio', oilgasconsumablefuelsSchema);
+const oilgasequipmentservices  = mongoose.model('oilgasequipmentservices_ratio', oilgasequipmentservicesSchema);
+const oilgasexplorationproduction  = mongoose.model('oilgasexplorationproduction_ratio', oilgasexplorationproductionSchema);
+const oilgasstoragetransportation  = mongoose.model('oilgasstoragetransportation_ratio', oilgasstoragetransportationSchema);
+const banks  = mongoose.model('banks_ratio', banksSchema);
+const brokeragecapitalmarkets  = mongoose.model('brokeragecapitalmarkets_ratio', brokeragecapitalmarketsSchema);
+const diversifiedfinancialcreditservices  = mongoose.model('diversifiedfinancialcreditservices_ratio', diversifiedfinancialcreditservicesSchema);
+const insurance  = mongoose.model('insurance_ratio', insuranceSchema);
+const regionalbanks  = mongoose.model('regionalbanks_ratio', regionalbanksSchema);
+const thriftsmortgagefinance  = mongoose.model('thriftsmortgagefinance_ratio', thriftsmortgagefinanceSchema);
+const biotechnology  = mongoose.model('biotechnology_ratio', biotechnologySchema);
+const healthcareservices  = mongoose.model('healthcareservices_ratio', healthcareservicesSchema);
+const medicalequipmentdevicessupplies  = mongoose.model('medicalequipmentdevicessupplies_ratio', medicalequipmentdevicessuppliesSchema);
+const pharmaceuticals  = mongoose.model('pharmaceuticals_ratio', pharmaceuticalsSchema);
+const aerospacedefense  = mongoose.model('aerospacedefense_ratio', aerospacedefenseSchema);
+const commercialprofessionalservices  = mongoose.model('commercialprofessionalservices_ratio', commercialprofessionalservicesSchema);
+const constructionmaterials  = mongoose.model('constructionmaterials_ratio', constructionmaterialsSchema);
+const industrialgoods  = mongoose.model('industrialgoods_ratio', industrialgoodsSchema);
+const machinery  = mongoose.model('machinery_ratio', machinerySchema);
+const transportation  = mongoose.model('transportation_ratio', transportationSchema);
+const hardwareequipment  = mongoose.model('hardwareequipment_ratio', hardwareequipmentSchema);
+const itservices  = mongoose.model('itservices_ratio', itservicesSchema);
+const semiconductors  = mongoose.model('semiconductors_ratio', semiconductorsSchema);
+const software  = mongoose.model('software_ratio', softwareSchema);
+const basicmaterials  = mongoose.model('basicmaterials_ratio', basicmaterialsSchema);
+const industrialmetals  = mongoose.model('industrialmetals_ratio', industrialmetalsSchema);
+const preciousmetals  = mongoose.model('preciousmetals_ratio', preciousmetalsSchema);
+const commercialreits  = mongoose.model('commercialreits_ratio', commercialreitsSchema);
+const realestateservices  = mongoose.model('realestateservices_ratio', realestateservicesSchema);
+const residentialmortgages  = mongoose.model('residentialmortgages_ratio', residentialmortgagesSchema);
+const retailreits  = mongoose.model('retailreits_ratio', retailreitsSchema);
+const electric  = mongoose.model('electric_ratio', electricSchema);
+const gas  = mongoose.model('gas_ratio', gasSchema);
+const independentpowerrenewables  = mongoose.model('independentpowerrenewables_ratio', independentpowerrenewablesSchema);
+const multilineutilities  = mongoose.model('multilineutilities_ratio', multilineutilitiesSchema);
+const water  = mongoose.model('water_ratio', waterSchema);
+const chemicals  = mongoose.model('chemicals_ratio', chemicalsSchema);
+const diversifiedconsumerservices  = mongoose.model('diversifiedconsumerservices_ratio', diversifiedconsumerservicesSchema);
 
 
 module.exports = {
-    ratiofinancials,
-    
-    
-  }
+  materials_ratio ,
+  consumer_staples_ratio ,
+  consumer_discretionary_ratio ,
+  energy_ratio ,
+  financials_ratio,
+  health_care_ratio,
+  industrials_ratio,
+  real_estate_ratio,
+  information_technology_ratio,
+  communication_services_ratio,
+  utilities_ratio,
+  interactivemediaentertainment,
+  telecom,
+  mediaentertainment,
+  apparelluxurygoods,
+  automobile,
+  hotelrestaurantleisure,
+  householddurables,
+  internetdirectmarketing,
+  leisuregoods,
+  retail,
+  foodbeveragetobacco,
+  foodstaplesretailing,
+  householdpersonalproducts,
+  oilgasconsumablefuels,
+  oilgasequipmentservices,
+  oilgasexplorationproduction,
+  oilgasstoragetransportation,
+  banks,
+  brokeragecapitalmarkets,
+  diversifiedfinancialcreditservices,
+  insurance,
+  regionalbanks,
+  thriftsmortgagefinance,
+  biotechnology,
+  healthcareservices,
+  medicalequipmentdevicessupplies,
+  pharmaceuticals,
+  aerospacedefense,
+  commercialprofessionalservices,
+  constructionmaterials,
+  industrialgoods,
+  machinery,
+  transportation,
+  hardwareequipment,
+  itservices,
+  semiconductors,
+  software,
+  basicmaterials,
+  industrialmetals,
+  preciousmetals,
+  commercialreits,
+  realestateservices,
+  residentialmortgages,
+  retailreits,
+  electric,
+  gas,
+  independentpowerrenewables,
+  multilineutilities,
+  water,
+  chemicals,
+  diversifiedconsumerservices
+  
+}
+
--- routes/ratio2.js
+++ routes/ratio2.js
@@ -1,13 +1,125 @@
 const router = require('express').Router();
-let ratio = require('../models/ratio2.model');
+let aggrid_pi = require('../models/ratio2.model');
 
-router.route('/financials').get((req, res) => {
-    ratio['ratiofinancials'].find().lean()
-        .then(spac => res.json(spac))
+
+
+router.route('/materials').get((req, res) => {
+    aggrid_pi['materials_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/consumer_staples').get((req, res) => {
+    aggrid_pi['consumer_staples_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/consumer_discretionary').get((req, res) => {
+    aggrid_pi['consumer_discretionary_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/energy').get((req, res) => {
+    aggrid_pi['energy_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/financials').get((req, res) => {
+    aggrid_pi['financials_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/health_care').get((req, res) => {
+    aggrid_pi['health_care_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/industrials').get((req, res) => {
+    aggrid_pi['industrials_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/real_estate').get((req, res) => {
+    aggrid_pi['real_estate_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/information_technology').get((req, res) => {
+    aggrid_pi['information_technology_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/communication_services').get((req, res) => {
+    aggrid_pi['communication_services_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/utilities').get((req, res) => {
+    aggrid_pi['utilities_ratio'].find().lean()
+        .then(aggrid_pi => res.json(aggrid_pi))
+        .catch(err => res.status(400).json('Error: ' + err));});
 
 
+
+router.route('/interactivemediaentertainment').get((req, res) => {aggrid_pi['interactivemediaentertainment'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/telecom').get((req, res) => {aggrid_pi['telecom'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/mediaentertainment').get((req, res) => {aggrid_pi['mediaentertainment'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/apparelluxurygoods').get((req, res) => {aggrid_pi['apparelluxurygoods'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/automobile').get((req, res) => {aggrid_pi['automobile'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/hotelrestaurantleisure').get((req, res) => {aggrid_pi['hotelrestaurantleisure'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/householddurables').get((req, res) => {aggrid_pi['householddurables'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/internetdirectmarketing').get((req, res) => {aggrid_pi['internetdirectmarketing'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/leisuregoods').get((req, res) => {aggrid_pi['leisuregoods'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/retail').get((req, res) => {aggrid_pi['retail'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/foodbeveragetobacco').get((req, res) => {aggrid_pi['foodbeveragetobacco'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/foodstaplesretailing').get((req, res) => {aggrid_pi['foodstaplesretailing'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/householdpersonalproducts').get((req, res) => {aggrid_pi['householdpersonalproducts'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/oilgasconsumablefuels').get((req, res) => {aggrid_pi['oilgasconsumablefuels'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/oilgasequipmentservices').get((req, res) => {aggrid_pi['oilgasequipmentservices'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/oilgasexplorationproduction').get((req, res) => {aggrid_pi['oilgasexplorationproduction'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/oilgasstoragetransportation').get((req, res) => {aggrid_pi['oilgasstoragetransportation'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/banks').get((req, res) => {aggrid_pi['banks'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/brokeragecapitalmarkets').get((req, res) => {aggrid_pi['brokeragecapitalmarkets'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/diversifiedfinancialcreditservices').get((req, res) => {aggrid_pi['diversifiedfinancialcreditservices'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/insurance').get((req, res) => {aggrid_pi['insurance'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/regionalbanks').get((req, res) => {aggrid_pi['regionalbanks'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/thriftsmortgagefinance').get((req, res) => {aggrid_pi['thriftsmortgagefinance'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/biotechnology').get((req, res) => {aggrid_pi['biotechnology'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/healthcareservices').get((req, res) => {aggrid_pi['healthcareservices'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/medicalequipmentdevicessupplies').get((req, res) => {aggrid_pi['medicalequipmentdevicessupplies'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/pharmaceuticals').get((req, res) => {aggrid_pi['pharmaceuticals'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/aerospacedefense').get((req, res) => {aggrid_pi['aerospacedefense'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/commercialprofessionalservices').get((req, res) => {aggrid_pi['commercialprofessionalservices'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/constructionmaterials').get((req, res) => {aggrid_pi['constructionmaterials'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/industrialgoods').get((req, res) => {aggrid_pi['industrialgoods'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/machinery').get((req, res) => {aggrid_pi['machinery'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/transportation').get((req, res) => {aggrid_pi['transportation'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/hardwareequipment').get((req, res) => {aggrid_pi['hardwareequipment'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/itservices').get((req, res) => {aggrid_pi['itservices'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/semiconductors').get((req, res) => {aggrid_pi['semiconductors'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/software').get((req, res) => {aggrid_pi['software'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/basicmaterials').get((req, res) => {aggrid_pi['basicmaterials'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/industrialmetals').get((req, res) => {aggrid_pi['industrialmetals'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/preciousmetals').get((req, res) => {aggrid_pi['preciousmetals'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/commercialreits').get((req, res) => {aggrid_pi['commercialreits'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/realestateservices').get((req, res) => {aggrid_pi['realestateservices'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/residentialmortgages').get((req, res) => {aggrid_pi['residentialmortgages'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/retailreits').get((req, res) => {aggrid_pi['retailreits'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/electric').get((req, res) => {aggrid_pi['electric'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/gas').get((req, res) => {aggrid_pi['gas'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/independentpowerrenewables').get((req, res) => {aggrid_pi['independentpowerrenewables'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/multilineutilities').get((req, res) => {aggrid_pi['multilineutilities'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/water').get((req, res) => {aggrid_pi['water'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/chemicals').get((req, res) => {aggrid_pi['chemicals'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
+router.route('/diversifiedconsumerservices').get((req, res) => {aggrid_pi['diversifiedconsumerservices'].find().lean().then(aggrid_pi =>res.json(aggrid_pi)).catch(err => res.status(400).json('Error: ' + err));});
 
 module.exports = router;
